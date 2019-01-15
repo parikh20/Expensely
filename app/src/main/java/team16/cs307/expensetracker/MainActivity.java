88e1274207f86873c76a493eba8c588b52d3305a
Jaineek Parikh <parikh20@purdue.edu>
2019-01-14 21:23:31 -0500

    Initial commit

---
+++ app/src/main/java/team16/cs307/expensetracker/MainActivity.java
@@ -0,0 +1,13 @@
+package team16.cs307.expensetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
