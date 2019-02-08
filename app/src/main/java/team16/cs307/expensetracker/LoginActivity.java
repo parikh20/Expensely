e4cef96fb6e6b29fedbb6535d91f17fdb66e6906
Jaineek Parikh <parikh20@purdue.edu>
2019-02-08 18:06:37 -0500

    Made basic Login Screen

---
+++ app/src/main/java/team16/cs307/expensetracker/LoginActivity.java
@@ -0,0 +1,74 @@
+package team16.cs307.expensetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.SignInButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // Get all attributes from the Login Activity
+    private EditText mUsername;
+    private EditText mPassword;
+    private TextView mForgotPasswordClickable;
+    private TextView mCreateNewAccountClickable;
+    private Button mLoginButton;
+    private SignInButton mGoogleSignInButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Get attributes
+        mUsername = (EditText) findViewById(R.id.username_exitText);
+        mPassword = (EditText) findViewById(R.id.password_editText);
+        mForgotPasswordClickable = (TextView) findViewById(R.id.forgot_password_clickable);
+        mCreateNewAccountClickable = (TextView) findViewById(R.id.create_new_account_clickable);
+        mLoginButton = (Button) findViewById(R.id.login_button);
+        mGoogleSignInButton = (SignInButton) findViewById(R.id.google_sign_in_button);
+
+        mGoogleSignInButton.setSize(SignInButton.SIZE_STANDARD);
+
+        // OnClickListener for when the login button is pressed
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // OnClickListener for when the Google sign in button is pressed
+        mGoogleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // OnClickListener for when the Forgot Password text is clicked
+        mForgotPasswordClickable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // OnClickListener for when the Create New Account text is clicked
+        mCreateNewAccountClickable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    // onClick to get rid of error message. Does nothing
+    public void onClick(View view) {
+    }
+}
