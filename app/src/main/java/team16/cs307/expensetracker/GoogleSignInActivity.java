42a62859de658d047dbad7ed787d86febc007ff6
Lucas Hall <hall333@purdue.edu>
2019-02-09 14:59:26 -0500

    Added Firebase Login Functionality, Placeholder Google Signin Page

---
+++ app/src/main/java/team16/cs307/expensetracker/GoogleSignInActivity.java
@@ -0,0 +1,13 @@
+package team16.cs307.expensetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoogleSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_sign_in);
+    }
+}
--- app/src/main/java/team16/cs307/expensetracker/LoginActivity.java
+++ app/src/main/java/team16/cs307/expensetracker/LoginActivity.java
@@ -1,7 +1,10 @@
 package team16.cs307.expensetracker;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,6 +12,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
     private TextView mCreateNewAccountClickable;
     private Button mLoginButton;
     private SignInButton mGoogleSignInButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +42,38 @@ public class LoginActivity extends AppCompatActivity {
         mLoginButton = (Button) findViewById(R.id.login_button);
         mGoogleSignInButton = (SignInButton) findViewById(R.id.google_sign_in_button);
 
+        mAuth = FirebaseAuth.getInstance();
+
+
         // OnClickListener for when the login button is pressed
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String username = mUsername.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if (username.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Please Enter Your Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Please Enter Your Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(username, password)
+                    .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()) {
+                                //TODO Launch login failure dialog or use toast
+                            } else {
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                LoginActivity.this.startActivity(intent);
+                                finish();
+                            }
+                        }
+                    });
 
             }
         });
@@ -45,7 +82,8 @@ public class LoginActivity extends AppCompatActivity {
         mGoogleSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(LoginActivity.this, GoogleSignInActivity.class);
+                LoginActivity.this.startActivity(intent);
             }
         });
 
@@ -65,6 +103,12 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currUser = mAuth.getCurrentUser();
+   }
+
 
     // onClick to get rid of error message. Does nothing
     public void onClick(View view) {
