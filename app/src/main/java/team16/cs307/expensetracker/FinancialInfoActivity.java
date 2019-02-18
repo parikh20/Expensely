ded1faf02fd325ba062911d714c2776fe231fc3f
Jaineek Parikh <parikh20@purdue.edu>
2019-02-17 22:50:19 -0500

    Removed superfluous XMLs

--- app/src/main/java/team16/cs307/expensetracker/FinancialInfoActivity.java
+++ app/src/main/java/team16/cs307/expensetracker/FinancialInfoActivity.java
@@ -8,6 +8,6 @@ public class FinancialInfoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_financial_info2);
+        setContentView(R.layout.activity_financial_info);
     }
 }
