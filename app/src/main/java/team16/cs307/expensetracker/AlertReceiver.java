81868978c9155fe699bdae4b6db404d11d2fc7ec
Lucas Hall <hall333@purdue.edu>
2019-04-11 17:02:21 -0400

    Working fix for alerts on oreo and above

--- app/src/main/java/team16/cs307/expensetracker/AlertReceiver.java
+++ app/src/main/java/team16/cs307/expensetracker/AlertReceiver.java
@@ -1,10 +1,12 @@
 package team16.cs307.expensetracker;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 
 public class AlertReceiver extends BroadcastReceiver {
@@ -16,6 +18,14 @@ public class AlertReceiver extends BroadcastReceiver {
         //called when alarm is triggered
         NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
 
+
+
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O &&
+                notificationManager.getNotificationChannel("BudgetAlert")==null) {
+            System.out.println("CORRECTING FOR OREO AND ABOVE");
+            notificationManager.createNotificationChannel(new NotificationChannel("BudgetAlert",
+                    "BudgetAl", NotificationManager.IMPORTANCE_DEFAULT));
+        }
         Notification notification = intent.getParcelableExtra(NOTIFICATION);
         int id = intent.getIntExtra(NOTIFICATION_ID, 0);
         notificationManager.notify(id, notification);
