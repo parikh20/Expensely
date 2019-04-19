9a1ac6cc6fb7040687c186e8af3cf4b811aef52d
Aakash Patel <patel615@purdue.edu>
2019-04-19 09:13:46 -0400

    NavBar - Strings and values
    Signed-off-by: Aakash Patel <patel615@purdue.edu>

--- app/src/main/java/team16/cs307/expensetracker/MainActivity.java
+++ app/src/main/java/team16/cs307/expensetracker/MainActivity.java
@@ -9,11 +9,15 @@ import android.graphics.Color;
 import android.graphics.ColorSpace;
 import android.os.SystemClock;
 import android.support.annotation.NonNull;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -106,6 +110,9 @@ public class MainActivity extends AppCompatActivity {
     private double amt;
     private String message;
 
+    //Drawer
+    private DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -127,6 +134,14 @@ public class MainActivity extends AppCompatActivity {
         becomeUser = findViewById(R.id.becomeUser);
         //mAlerts = findViewById(R.id.main_alerts);
         message = "0";
+
+        //Navigation Bar:
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
 
         //NOTE TO LUCAS REMEMBER TO REMOVE THIS!!!!!!!!!!!!!!!!!!!!!!!  Testing only!!!!!!!!!! TODO
@@ -610,6 +625,16 @@ public class MainActivity extends AppCompatActivity {
     void alerts() {
         Intent intent = new Intent(getApplicationContext(), AlertPreferencesActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 }
 
